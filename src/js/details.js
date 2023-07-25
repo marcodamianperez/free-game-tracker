0542c64b593654547d85b52d06a0521289c259d0
Marco Pérez <marcodamianperez@gmail.com>
2023-07-25 15:30:37 -0300

    mayor update. Refactor project to utilize React for enhanced scalability and maintainability as the application grows

--- src/js/details.js
+++
@@ -1,5 +0,0 @@
-import { getDetails, handlerDetails } from "./functions.js";
-import { cardsContainer } from "./globals.js";
-
-getDetails();
-cardsContainer.addEventListener("click", (e) => handlerDetails(e));
