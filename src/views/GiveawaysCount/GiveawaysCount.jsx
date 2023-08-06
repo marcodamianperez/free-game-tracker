1e89f4e119203345dd6bffbc6c46645ff31a0ae7
Marco Pérez <marcodamianperez@gmail.com>
2023-08-06 19:10:32 -0300

    add functionality to GiveawaysCount component to retrieve and display quantity info

--- src/views/GiveawaysCount/GiveawaysCount.jsx
+++ src/views/GiveawaysCount/GiveawaysCount.jsx
@@ -1,7 +1,46 @@
-import styles from "./GiveawaysCount.module.scss";
+import { useState, useEffect } from "react";
 import { BsController, BsPuzzle, BsGem } from "react-icons/bs";
+import styles from "./GiveawaysCount.module.scss";
 
 const GiveawaysCount = () => {
+  const [count, setCount] = useState({});
+
+  const URL = "https://gamerpower.p.rapidapi.com/api/";
+  const query = "giveaways";
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": API_KEY,
+      "X-RapidAPI-Host": "gamerpower.p.rapidapi.com",
+    },
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${URL}${query}`, options);
+        const giveaways = await response.json();
+
+        const gameCount = giveaways.filter(
+          (giveaway) => giveaway.type.toLowerCase() === "game"
+        ).length;
+        const dlcCount = giveaways.filter(
+          (giveaway) => giveaway.type.toLowerCase() === "dlc"
+        ).length;
+        const betaGameCount = giveaways.filter(
+          (giveaway) => giveaway.type.toLowerCase() === "early access"
+        ).length;
+
+        setCount({ gameCount, dlcCount, betaGameCount });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <section className={styles.giveawaysCount}>
       <div className={styles.uContainer}>
@@ -13,15 +52,15 @@ const GiveawaysCount = () => {
 
         <p className={styles.giveawaysCountText}>
           <BsController />
-          <span id="qtyGames">179</span>&nbsp;Games
+          <span id="qtyGames">{count.gameCount}</span>&nbsp;Games
         </p>
         <p className={styles.giveawaysCountText}>
           <BsPuzzle />
-          <span id="qtyDlc">201</span>&nbsp;DLC's
+          <span id="qtyDlc">{count.dlcCount}</span>&nbsp;DLC's
         </p>
         <p className={styles.giveawaysCountText}>
           <BsGem />
-          <span id="qtyBeta">45</span>&nbsp;Beta Games
+          <span id="qtyBeta">{count.betaGameCount}</span>&nbsp;Beta Games
         </p>
       </div>
     </section>
